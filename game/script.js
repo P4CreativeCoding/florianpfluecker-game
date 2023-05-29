5f84aeabb90e8981bdf3ad0501bca8490ebe8056
florianpfluecker <73837515+florianpfluecker@users.noreply.github.com>
2023-05-29 21:28:48 +0200

    update

--- game/script.js
+++ game/script.js
@@ -1,41 +1,66 @@
-document.addEventListener("keydown", function (event) {
-  var car = document.getElementById("car");
-  var container = document.querySelector(".container");
-  var containerWidth = container.offsetWidth;
-  var containerHeight = container.offsetHeight;
-  var carWidth = car.offsetWidth;
-  var carHeight = car.offsetHeight;
-  var carPosX = parseInt(car.style.left) || 0;
-  var carPosY = parseInt(car.style.top) || 0;
-  var stepSize = 10;
+var car = document.getElementById("car");
+var container = document.querySelector(".container");
+var containerWidth = container.offsetWidth;
+var containerHeight = container.offsetHeight;
+var carWidth = car.offsetWidth;
+var carHeight = car.offsetHeight;
+var carPosX = containerWidth / 2 - carWidth / 2;
+var carPosY = containerHeight / 2 - carHeight / 2;
+var stepSize = 1;
+var speedX = 0;
+var speedY = 0;
+var acceleration = 0.2;
+var friction = 0.98;
+var keys = {};
 
-  switch (event.keyCode) {
-    case 37: // Left arrow key
-      carPosX -= stepSize;
-      if (carPosX < 0) {
-        carPosX = 0;
-      }
-      break;
-    case 38: // Up arrow key
-      carPosY -= stepSize;
-      if (carPosY < 0) {
-        carPosY = 0;
-      }
-      break;
-    case 39: // Right arrow key
-      carPosX += stepSize;
-      if (carPosX + carWidth > containerWidth) {
-        carPosX = containerWidth - carWidth;
-      }
-      break;
-    case 40: // Down arrow key
-      carPosY += stepSize;
-      if (carPosY + carHeight > containerHeight) {
-        carPosY = containerHeight - carHeight;
-      }
-      break;
+function update() {
+  if (keys[37]) {
+    // Left arrow key
+    speedX -= acceleration;
+  }
+  if (keys[38]) {
+    // Up arrow key
+    speedY -= acceleration;
+  }
+  if (keys[39]) {
+    // Right arrow key
+    speedX += acceleration;
+  }
+  if (keys[40]) {
+    // Down arrow key
+    speedY += acceleration;
+  }
+
+  speedX *= friction;
+  speedY *= friction;
+  carPosX += speedX;
+  carPosY += speedY;
+
+  if (carPosX < 0) {
+    carPosX = 0;
+  }
+  if (carPosY < 0) {
+    carPosY = 0;
+  }
+  if (carPosX + carWidth > containerWidth) {
+    carPosX = containerWidth - carWidth;
+  }
+  if (carPosY + carHeight > containerHeight) {
+    carPosY = containerHeight - carHeight;
   }
 
   car.style.left = carPosX + "px";
   car.style.top = carPosY + "px";
+
+  requestAnimationFrame(update);
+}
+
+document.addEventListener("keydown", function (event) {
+  keys[event.keyCode] = true;
 });
+
+document.addEventListener("keyup", function (event) {
+  keys[event.keyCode] = false;
+});
+
+update();
