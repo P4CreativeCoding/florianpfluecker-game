2c54a89d719190ef121ad1f65b934f8669dc68ef
florianpfluecker <73837515+florianpfluecker@users.noreply.github.com>
2023-05-29 21:15:33 +0200

    aufräumen

--- game/script.js
+++ game/script.js
@@ -1,109 +1,40 @@
-document.addEventListener("keydown", handleKeyDown);
-document.addEventListener("keyup", handleKeyUp);
+document.addEventListener("keydown", function (event) {
+  var ball = document.getElementById("ball");
+  var container = document.querySelector(".container");
+  var containerWidth = container.offsetWidth;
+  var containerHeight = container.offsetHeight;
+  var ballSize = ball.offsetWidth;
+  var ballPosX = parseInt(ball.style.left) || 0;
+  var ballPosY = parseInt(ball.style.top) || 0;
+  var stepSize = 10;
 
-var acceleration = 0.2; // Beschleunigung des Autos
-var deceleration = -0.2; // Bremsen des Autos
-var maxSpeed = 5; // Maximale Geschwindigkeit des Autos
-
-var speedX = 0; // Geschwindigkeit des Autos in horizontaler Richtung
-var speedY = 0; // Geschwindigkeit des Autos in vertikaler Richtung
-
-function handleKeyDown(event) {
-  var key = event.key;
-
-  if (key === "ArrowUp") {
-    event.preventDefault(); // Verhindert das Scrollen der Seite bei Pfeiltastenbetätigung
-    accelerateUp();
-  } else if (key === "ArrowDown") {
-    event.preventDefault();
-    accelerateDown();
-  } else if (key === "ArrowLeft") {
-    event.preventDefault();
-    accelerateLeft();
-  } else if (key === "ArrowRight") {
-    event.preventDefault();
-    accelerateRight();
+  switch (event.keyCode) {
+    case 37: // Left arrow key
+      ballPosX -= stepSize;
+      if (ballPosX < 0) {
+        ballPosX = 0;
+      }
+      break;
+    case 38: // Up arrow key
+      ballPosY -= stepSize;
+      if (ballPosY < 0) {
+        ballPosY = 0;
+      }
+      break;
+    case 39: // Right arrow key
+      ballPosX += stepSize;
+      if (ballPosX + ballSize > containerWidth) {
+        ballPosX = containerWidth - ballSize;
+      }
+      break;
+    case 40: // Down arrow key
+      ballPosY += stepSize;
+      if (ballPosY + ballSize > containerHeight) {
+        ballPosY = containerHeight - ballSize;
+      }
+      break;
   }
-}
 
-function handleKeyUp(event) {
-  var key = event.key;
-
-  if (key === "ArrowUp" || key === "ArrowDown") {
-    event.preventDefault();
-    stopVertical();
-  } else if (key === "ArrowLeft" || key === "ArrowRight") {
-    event.preventDefault();
-    stopHorizontal();
-  }
-}
-
-function accelerateUp() {
-  speedY -= acceleration;
-  if (speedY < -maxSpeed) {
-    speedY = -maxSpeed;
-  }
-}
-
-function accelerateDown() {
-  speedY += acceleration;
-  if (speedY > maxSpeed) {
-    speedY = maxSpeed;
-  }
-}
-
-function accelerateLeft() {
-  speedX -= acceleration;
-  if (speedX < -maxSpeed) {
-    speedX = -maxSpeed;
-  }
-}
-
-function accelerateRight() {
-  speedX += acceleration;
-  if (speedX > maxSpeed) {
-    speedX = maxSpeed;
-  }
-}
-
-function stopVertical() {
-  if (speedY > 0) {
-    speedY += deceleration;
-    if (speedY < 0) {
-      speedY = 0;
-    }
-  } else if (speedY < 0) {
-    speedY -= deceleration;
-    if (speedY > 0) {
-      speedY = 0;
-    }
-  }
-}
-
-function stopHorizontal() {
-  if (speedX > 0) {
-    speedX += deceleration;
-    if (speedX < 0) {
-      speedX = 0;
-    }
-  } else if (speedX < 0) {
-    speedX -= deceleration;
-    if (speedX > 0) {
-      speedX = 0;
-    }
-  }
-}
-
-function updatePosition() {
-  var auto = document.getElementById("auto");
-  var currentTop = parseInt(auto.style.top) || 0;
-  var currentLeft = parseInt(auto.style.left) || 0;
-
-  var newTop = currentTop + speedY;
-  var newLeft = currentLeft + speedX;
-
-  auto.style.top = newTop + "px";
-  auto.style.left = newLeft + "px";
-}
-
-setInterval(updatePosition, 16); // Aktualisiert die Position des Autos alle 16 Millisekunden
+  ball.style.left = ballPosX + "px";
+  ball.style.top = ballPosY + "px";
+});
